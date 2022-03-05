4a2bbf223ce1a8c708ee94b28467d97eae3dd8c2
jay <rladudrnjs12@gmail.com>
2022-03-05 15:12:19 +0900

    set Serverside rendering

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,11 @@
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
-const Home = () => {
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    (async () => {
-      const { results } = await (await fetch(`/api/movies`)).json();
-      setMovies(results);
-    })();
-  }, []);
+const Home = ({ results }) => {
   return (
     <div>
       <Seo title="Home" />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie) => (
+      {results?.map((movie) => (
         <div className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
@@ -45,3 +36,15 @@ const Home = () => {
   );
 };
 export default Home;
+
+// only excute serverside function
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
+}
