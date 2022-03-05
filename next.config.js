663bab399c1f826f74e3112c51b4746a9cdcc7fa
jay <rladudrnjs12@gmail.com>
2022-03-05 13:41:21 +0900

    next.config redirect, rewrite

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    // redirect: a로 들어왔을 때 b로 다시 보내준다.
+    return [
+      {
+        // source: "/contact",
+        // destination: "/form",
+        source: "/old-page/:path*",
+        destination: "/new-page/:path*",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    // rewrites: a로 들어왔을 때 b로 다시 보내주지만 url은 바뀌지 않는다.
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
